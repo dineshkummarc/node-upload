00345c3f4037ccd3f9045657bddcc3f7d99027c6
Kristian Hellquist and Richard Johansson <dev+kristian+richard@mynewsdesk.com>
2011-06-15 14:29:22 +0200

    wip

--- web.js
+++ web.js
@@ -4,8 +4,16 @@
 
 var express = require('express');
 var formidable = require('formidable');
-
 var fs = require('fs');
+var knox = require('knox');
+
+var client = knox.createClient({
+    key: '1YFPB3MTZ5K1GMZ1FJ02', 
+    secret: 'je7VPPY0cupCQPdtuG1b8tsD0c7wkACxfaakzoQj', 
+    bucket: 'node-test',
+    port: 443
+   
+});
 
 var app = express.createServer();
 app.use(express.static(__dirname + '/public'));
@@ -25,7 +33,39 @@ app.post('/video', function(req, res, next) {
 			next(err);
 		} else {
 			console.log('\nuploaded %s to %s', files.video.filename, files.video.path);
-			fs.rename(files.video.path,"./public/" + files.video.filename);
+			
+			// send file to amazon
+            fs.readFile(files.video.path, function(err, buf){
+              if(err){
+                  console.log("error:");
+                  console.dir(err)
+                  next();
+              }
+             console.log("Send file to amazon");
+              var amazon_req = client.put(files.video.path, {
+                  'Content-Length': buf.length
+                , 'Content-Type': 'text/plain'
+              });
+              amazon_req.on('response', function(amazon_res){
+                  console.log("amazone §xxxxxxxxxxxxxxxxxx");
+                  console.log(amazon_res.statusCode);
+                  console.log(amazon_res.headers);
+                  console.log(amazon_res.body);
+                  
+                if (200 == amazon_res.statusCode) {
+                  console.log('saved to amazon on: %s', amazon_req.url);
+                }
+              });
+
+              amazon_req.on('error', function(err){
+                  console.log('amazon error: %s', err);
+                  next();
+              });
+              console.log("amazon request");
+              console.dir(amazon_req);
+              amazon_req.end(buf);
+            });
+            fs.rename(files.video.path, "./public/" + files.video.filename);    
 			res.redirect(files.video.filename);
 		}
 	});
