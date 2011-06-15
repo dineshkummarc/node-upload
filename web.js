c7598a71f24573f21ae10c0c0ae0ca5f3c11c5a7
Kristian Hellquist and Richard Johansson <dev+kristian+richard@mynewsdesk.com>
2011-06-15 16:14:05 +0200

    Redirect user when file is read on server

--- web.js
+++ web.js
@@ -8,74 +8,69 @@ var fs = require('fs');
 var knox = require('knox');
 
 var client = knox.createClient({
-    key: '1YFPB3MTZ5K1GMZ1FJ02', 
-    secret: 'je7VPPY0cupCQPdtuG1b8tsD0c7wkACxfaakzoQj', 
-    bucket: 'node-test',
-    port: 443
-   
+  key: 'AKIAJBLFFICH3KAG7HJQ',
+  secret: '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',
+  bucket: 'node-test'
+
 });
 
 var app = express.createServer();
 app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res){
-    res.redirect('/demo-form');
+app.get('/', function(req, res) {
+  res.redirect('/demo-form');
 })
 
 app.get('/demo-form', function(req, res) {
-	res.send('<form action="/video" method="post" enctype="multipart/form-data">' + '<p>Image: <input type="file" name="video" /></p>' + '<p><input type="submit" value="Upload" /></p>' + '</form>');
+  res.send('<form action="/video" method="post" enctype="multipart/form-data">' + '<p>Image: <input type="file" name="video" /></p>' + '<p><input type="submit" value="Upload" /></p>' + '</form>');
 });
 
 app.post('/video', function(req, res, next) {
 
-	var form = new formidable.IncomingForm();
-	form.parse(req, function(err, fields, files) {
-		if (err) {
-			next(err);
-		} else {
-			console.log('\nuploaded %s to %s', files.video.filename, files.video.path);
-			
-			// send file to amazon
-            fs.readFile(files.video.path, function(err, buf){
-              if(err){
-                  console.log("error:");
-                  console.dir(err)
-                  next();
-              }
-             console.log("Send file to amazon");
-              var amazon_req = client.put(files.video.path, {
-                  'Content-Length': buf.length
-                , 'Content-Type': 'text/plain'
-              });
-              amazon_req.on('response', function(amazon_res){
-                  console.log("amazone §xxxxxxxxxxxxxxxxxx");
-                  console.log(amazon_res.statusCode);
-                  console.log(amazon_res.headers);
-                  console.log(amazon_res.body);
-                  
-                if (200 == amazon_res.statusCode) {
-                  console.log('saved to amazon on: %s', amazon_req.url);
-                }
-              });
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+      var path_to_s3 = "init";
+    if (err) {
+      next(err);
+    } else {
+      console.log('\nuploaded %s to %s', files.video.filename, files.video.path);
+      // send file to amazon
+      fs.readFile(files.video.path, function(err, buf) {
+        console.log("Send file to amazon");
+          path_to_s3 =  files.video.path;
+          console.log("path_to_s3 %s", path_to_s3);
+        var amazon_req = client.put(path_to_s3, {
+          'Content-Length': buf.length,
+          'Content-Type': 'text/plain'
+        });
 
-              amazon_req.on('error', function(err){
-                  console.log('amazon error: %s', err);
-                  next();
-              });
-              console.log("amazon request");
-              console.dir(amazon_req);
-              amazon_req.end(buf);
-            });
-            fs.rename(files.video.path, "./public/" + files.video.filename);    
-			res.redirect(files.video.filename);
-		}
-	});
+        fs.rename(files.video.path, "./public/" + files.video.filename);
+          console.log("Redirect user with path to s3 %s", path_to_s3);
+          res.redirect(files.video.filename);
 
-	// We can add listeners for several form
-	// events such as "progress"
-	form.on('progress', function(bytesReceived, bytesExpected) {
-		var percent = (bytesReceived / bytesExpected * 100) | 0;
-		console.log("Got %s of %s bytes", bytesReceived, bytesExpected);
-	});
+
+        amazon_req.on('response', function(amazon_res) {
+          amazon_res.on('data', function(chunk) {
+            console.log('amazon chunk: %s', chunk);
+          });
+
+          if (200 == amazon_res.statusCode) {
+            console.log('saved to amazon on: %s', amazon_req.url);
+                    }
+        });
+
+        amazon_req.on('error', function(err) {
+          console.log('amazon error: %s', err);
+        });
+        amazon_req.end(buf);
+      });
+    }
+  });
+
+  // We can add listeners for several form
+  // events such as "progress"
+  form.on('progress', function(bytesReceived, bytesExpected) {
+    var percent = (bytesReceived / bytesExpected * 100) | 0;
+  });
 
 });
 
